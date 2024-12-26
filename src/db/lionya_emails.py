7ddaaf43b33d20401ea92f0681b7ad38cb2f1230
Ray Bunnage <bunnage.ray@gmail.com>
2024-12-26 10:26:35 -0800

    first try at lionyas emails

--- src/db/lionya_emails.py
+++ src/db/lionya_emails.py
@@ -17,10 +17,32 @@ class LionyaEmails:
     def say_hello_world(self):
         print("Hello, World!")
 
+    def get_all_lionya_emails(self):
+        try:
+            fields = [
+                "id",
+                "email_address",
+                "email_count",
+            ]
+            response = self.supabase.select_from_table(
+                "lionya_emails", fields
+            )
+            if response and len(response) > 0:
+                return response
+            else:
+                print("Failed to retrieve expert fields or policy prevented read.")
+                return None
+
+        except Exception as e:
+            print(f"Error getting expert fields: {str(e)}")
+            return None
+
+
 if __name__ == "__main__":
     load_dotenv()
     supabase_url = os.getenv("SUPABASE_URL")
     supabase_key = os.getenv("SUPABASE_KEY")
     supabase_client = SupabaseService(supabase_url, supabase_key)
     lionya_emails = LionyaEmails(supabase_client)
-    lionya_emails.say_hello_world()
+    # lionya_emails.say_hello_world()
+    print(lionya_emails.get_all_lionya_emails())
